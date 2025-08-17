58ae400a82550e818d28d83969f7e3e253319101
chanhwi <kangchan1997@gmail.com>
2025-08-18 02:30:22 +0900

    일타싸피 정리

---
+++ 연습장/billiard.py
@@ -0,0 +1,22 @@
+'''
+1. 세기와 각도 두값을 인자로 전달하여 진행
+- 거리 : 피타고라스의 정리
+- 각도 : 삼각함수, 라디안
+
+2. 홀은 좌표상 표기된 위치에 있음
+
+3. 반발계수와 마찰계수는 직접 플레이해가면서 수치 조절
+
+'''
+```python
+import math
+# 1 radian = 180 / math.pi (degree)          math.degrees()
+# 1 degree = math.pi / 180 (radian)          math.radians()
+x = y = 2
+ceta = math.atan(y/x) = math.asin(y/r) = math.acos(x/r)
+
+ssafy_angle = (-theta -90) % 360
+theta = math.atan2(dy, dx)
+dy = y2 - y1
+dx = x2 - x1
+```
